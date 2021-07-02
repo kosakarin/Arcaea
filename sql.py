959c448f25771f6da571b7788167d51f7edabb57
Yuri-YuzuChaN <65105396+Yuri-YuzuChaN@users.noreply.github.com>
2021-07-02 17:34:57 +0800

    Update sql.py

--- sql.py
+++ sql.py
@@ -15,15 +15,38 @@ class arcsql():
                 ID          INTEGER     PRIMARY KEY     NOT NULL,
                 QQID        INTEGER     NOT NULL,
                 ARCID       INTEGER     NOT NULL,
-                ARCNAME     TEXT        NOT NULL
+                ARCNAME     TEXT        NOT NULL,
+                USER_ID     INTEGER,
+                BIND_ID     INTEGER
             )''')
         except Exception as e:
             print(e)
     
-    def insert_user(self, qqid, arcid, arcname):
+    def makelogin(self):
+        try:
+            self.conn().execute('''CREATE TABLE LOGIN(
+                ID          INTEGER     PRIMARY KEY     NOT NULL,
+                BIND_ID     INTEGER     NOT NULL,
+                EMAIL       TEXT        NOT NULL,
+                PASSWORD    TEXT        NOT NULL
+            )''')
+        except Exception as e:
+            print(e)
+    
+    def insert_temp_user(self, qqid, arcid, arcname):
         try:
             conn = self.conn()
-            conn.execute(f'INSERT INTO USER VALUES (NULL, {qqid}, {arcid}, "{arcname}")')
+            conn.execute(f'INSERT INTO USER VALUES (NULL, {qqid}, {arcid}, "{arcname.lower()}", NULL, NULL)')
+            conn.commit()
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+    def insert_user(self, arcname, user_id, bind_id):
+        try:
+            conn = self.conn()
+            conn.execute(f'UPDATE USER SET ARCNAME = "{arcname}", USER_ID = {user_id}, BIND_ID = {bind_id} WHERE ARCNAME = "{arcname}"')
             conn.commit()
             return True
         except Exception as e:
@@ -32,23 +55,39 @@ class arcsql():
 
     def get_user_id(self, qqid):
         try:
-            result = self.conn().execute(f'SELECT ARCID FROM USER WHERE QQID = {qqid}').fetchall()
+            result = self.conn().execute(f'SELECT USER_ID FROM USER WHERE QQID = {qqid}').fetchall()
             return result
         except Exception as e:
             print(e)
             return False
 
-    def get_user_name(self, qqid):
+    def get_user_name(self, user_id):
         try:
-            result = self.conn().execute(f'SELECT ARCNAME FROM USER WHERE QQID = {qqid}').fetchall()
+            result = self.conn().execute(f'SELECT ARCNAME FROM USER WHERE USER_ID = {user_id}').fetchall()
+            return result
+        except Exception as e:
+            print(e)
+            return False
+
+    def get_bind_id(self, user_id):
+        try:
+            result = self.conn().execute(f'SELECT ARCID, BIND_ID FROM USER WHERE USER_ID = {user_id}').fetchall()
+            return result
+        except Exception as e:
+            print(e)
+            return False
+
+    def get_all_login(self):
+        try:
+            result = self.conn().execute(f'SELECT BIND_ID, EMAIL, PASSWORD FROM LOGIN').fetchall()
             return result
         except Exception as e:
             print(e)
             return False
     
-    def get_user_all(self, arcid):
+    def get_login(self, bind_id):
         try:
-            result = self.conn().execute(f'SELECT ARCID, ARCNAME FROM USER WHERE ARCID = {arcid}').fetchall()
+            result = self.conn().execute(f'SELECT EMAIL, PASSWORD FROM LOGIN WHERE BIND_ID = {bind_id}').fetchall()
             return result
         except Exception as e:
             print(e)
