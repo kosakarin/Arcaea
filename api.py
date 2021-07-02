3bec96dc84bdf5129475bb953ff56f05dd47269a
Yuri-YuzuChaN <65105396+Yuri-YuzuChaN@users.noreply.github.com>
2021-07-02 17:36:00 +0800

    Update api.py

--- api.py
+++ api.py
@@ -1,13 +1,17 @@
 import aiohttp
 
-api = 'http://yuzuai.cn:2345'
+api = 'http://xxxxx.cn'
+url = 'https://webapi.lowiro.com/'
+me = 'webapi/user/me'
+login = 'auth/login'
+
 
 async def get_api_info(project, arcid=0, diff=2, limit=0, song=None):
     try:
         if project == 'bind':
             url = f'{api}/v4/user/info?user={arcid}&usercode={arcid}&recent={limit}'
         elif project == 'info':
-            url = f'{api}/v4/user/best30?user={arcid}&usercode={arcid}&overflow=0'
+            url = f'{api}/v4/user/best30?user={arcid}&usercode={arcid}'
         elif project == 'recent':
             url = f'{api}/v4/user/info?user={arcid}&usercode={arcid}&recent=1'
         elif project == 'score':
@@ -25,3 +29,13 @@ async def get_api_info(project, arcid=0, diff=2, limit=0, song=None):
                 return await req.json()
     except Exception as e:
         return e
+
+async def get_web_api(email, password):
+    data = {'email': email, 'password': password}
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url + login, data=data) as req:
+            await req.json()
+
+        async with session.get(url + me) as reqs:
+            return await reqs.json()
+            
