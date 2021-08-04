356b34c1edf70965f849ce9cc0c0af536288429a
Yuri-YuzuChaN <65105396+Yuri-YuzuChaN@users.noreply.github.com>
2021-08-04 15:19:45 +0800

    添加est查分器

--- api.py
+++ api.py
@@ -1,14 +1,38 @@
-import aiohttp
+import aiohttp, websockets, json, brotli
 
 url = 'https://webapi.lowiro.com/'
 me = 'webapi/user/me'
 login = 'auth/login'
+est = 'wss://arc.estertion.win:616/'
 
-async def get_web_api(email, password):
+async def get_web_api(email, password, ac: bool = False):
     data = {'email': email, 'password': password}
     async with aiohttp.ClientSession() as session:
         async with session.post(url + login, data=data) as req:
-            await req.json()
+            if ac:
+                ok = True if req.status == 200 else False
+                return ok 
 
         async with session.get(url + me) as reqs:
             return await reqs.json()
+
+async def arcb30(arcid: str, re: bool = False):
+    try:
+        b30_data = []
+        async with websockets.connect(est) as ws:
+            await ws.send(str(arcid))
+            await ws.recv()
+            while True:
+                if ws.closed:
+                    break
+                data = await ws.recv()
+                if data == 'error,add':
+                    return '连接查分器错误'
+                elif data == 'bye':
+                    return b30_data
+                elif re and isinstance(data, bytes):
+                    return json.loads(brotli.decompress(data))
+                elif isinstance(data, bytes):
+                    b30_data.append(json.loads(brotli.decompress(data)))
+    except Exception as e:
+        return f'Error: {type(e)}'
